4e8faee1f9fbf07e596587a7517a63d6a2c2ef1a
Monjurulhasan <monjurul.hasan98@gmail.com>
2018-04-10 09:33:10 +0600

    PHPvariable

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+// There are two types of php variable..
+echo "<h3>There are two types of php vadriable:</h3>";
+// 1. Predefine variable
+echo "<h4>Predefine variable</h4>";
+echo 'GLOBALS <br> _SERVER <br> _GET <br> _POST <br> _FILES <br> _COOKIE <br> _SESSION <br> _REQUEST <br> _ENV';
+echo "<br>";
+
+// 2. Userdefine variable
+echo "<h4>Userdefine variable</h4>";
+echo "1. Variable scope <br>";
+echo "2. Variable variables <br>";
+echo "3. Referance variable <br>";
